8a860b77cf014f4a3857e17034383fee0ae22b65
Matthew <matthew6@mac2.local>
2017-11-11 12:19:10 -0500

    update

--- roll.py
+++ roll.py
@@ -2,20 +2,12 @@ from random import random
 from time import sleep
 
 def Stat(xd):
-    global lol
     lol = round(random()*xd)
-    return print(lol)
+    return lol
 
-while True:
-    Stat(10)
-    sleep(0.005)
-    if lol == 6:
-        sleep(1)
-    Stat(100)
-    sleep(0.005)
-    if lol == 66:
-        sleep(1)
-    Stat(1000)
-    sleep(0.005)
-    if lol == 666:
-        sleep(1)
+
+def Name():
+    name = input("> ")
+    print(name, "has ", Stat(1000), " XP")
+
+Name()
